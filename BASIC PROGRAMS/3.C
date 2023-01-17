b4a67f9fd3f1cb6d58b1f6236ab2a30c41780f59
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:15:55 +0530

    Create 3.C

---
+++ BASIC PROGRAMS/3.C
@@ -0,0 +1,12 @@
+//Program to enter two numbers and perform all basic operations
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   scanf("%d%d",&a,&b);
+   printf("sum=%d\n",a+b);
+   printf("product=%d\n",a*b);
+   printf("subtract=%d\n",a-b);
+   printf("div=%d\n",a/b);
+   printf("remainder=%d\n",a%b);
+}

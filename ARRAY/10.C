4c3b2c69b44999223b20cee502f531b6c2662333
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 18:32:21 +0530

    Create 10.C

---
+++ ARRAY/10.C
@@ -0,0 +1,26 @@
+//Program to find lower triangular matrix.
+int main()
+{
+    int r,sc=0,i,j;
+    scanf("%d",&r);
+    int a[r][r];
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<r;j++)
+       {
+           scanf("%d",&a[i][j]);
+       }
+       printf("\n");
+    }
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<r;j++)
+       {
+           if(j<=i)
+           printf("%d",a[i][j]);
+           else
+           printf(" ");
+       }
+       printf("\n");
+    }
+}

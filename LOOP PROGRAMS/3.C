e55dbfc072a08f227803b08b37af5f6a352d48c8
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:46:03 +0530

    Create 3.C

---
+++ LOOP PROGRAMS/3.C
@@ -0,0 +1,11 @@
+//Program to print all alphabets a to z using while loop
+#include<stdio.h>
+void main()
+{
+  char n='a';
+  while(n>='a'&& n<='z')
+  {
+      printf("%c ",n);
+      n++;
+  }
+ }

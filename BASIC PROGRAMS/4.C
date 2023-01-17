fb6e489666750832c11669ade0de99deece95ed1
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:17:00 +0530

    Create 4.C

---
+++ BASIC PROGRAMS/4.C
@@ -0,0 +1,8 @@
+//Program to enter length and breadth of a rectangle and find its perimeter.
+#include<stdio.h>
+int main()
+{
+   int l,b;
+   scanf("%d%d",&l,&b);
+   printf("perimeter=%d\n",2*(l+b));
+}

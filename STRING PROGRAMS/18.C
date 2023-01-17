48762d2db579eef74b10622d23f185f59b26f944
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:08:27 +0530

    Create 18.C

---
+++ STRING PROGRAMS/18.C
@@ -0,0 +1,22 @@
+//Program to remove all occurrences of a character in a string.
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    char j;
+    int i,p,k=0;
+    scanf("%[^\n]s",a);
+    scanf(" %c",&j);
+      for(i=0;a[i];i++)
+      k++;
+    for(i=0;a[i];i++)
+    {
+        if(a[i]==j)
+        {
+       for(p=i;a[p];p++)
+        a[p]=a[p+1];
+        k--;
+        }
+    }
+    printf("%s",a);
+}

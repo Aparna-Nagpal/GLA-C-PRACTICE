ca9e0b0932aa5b1038c185a3e7729fc0d12a8da6
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:39:36 +0530

    Create 10.C

---
+++ if....else PROGRAMS/10.C
@@ -0,0 +1,23 @@
+//Program to input week number and print week day.
+#include<stdio.h>
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   if(a==0)
+   printf("Sunday");
+   else if(a==1)
+   printf("Monday");
+    else if(a==2)
+   printf("Tuesday");
+    else if(a==3)
+   printf("Wednesday");
+    else if(a==4)
+   printf("Thursday");
+   else if(a==5)
+   printf("Friday");
+    else if(a==6)
+   printf("Saturday");
+    else
+    printf("enter a valid number");
+  }

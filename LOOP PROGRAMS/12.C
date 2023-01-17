b28236e0520d302d5504aa9b6076939246ac767f
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:49:56 +0530

    Create 12.C

---
+++ LOOP PROGRAMS/12.C
@@ -0,0 +1,15 @@
+//Program to find sum of first and last digit of any number.
+#include<stdio.h>
+void main()
+{
+  int a,s=0,p=1,g;
+  scanf("%d",&a);
+  g=a;
+  while(a>0)
+  {
+      s++;
+      a=a/10;
+      p=p*10;
+  }
+  printf("%d",g/(p/10)+g%10);
+ }

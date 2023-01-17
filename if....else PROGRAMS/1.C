326574f32f2794c5cfab6f88cbab2685c923175c
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:35:40 +0530

    Create 1.C

---
+++ if....else PROGRAMS/1.C
@@ -0,0 +1,11 @@
+//Program to find maximum between two numbers.
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   scanf("%d%d",&a,&b);
+   if(a>b)
+   printf("%d",a);
+   else
+   printf("%d",b);
+}

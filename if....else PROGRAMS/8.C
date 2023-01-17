8c62e9346ddbc154b7468e28c4eb0f3e73cb9928
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:38:40 +0530

    Create 8.C

---
+++ if....else PROGRAMS/8.C
@@ -0,0 +1,11 @@
+//Program to input an alphabet and check whether it is vowel or consonant.
+#include<stdio.h>
+int main()
+{
+   char a;
+   scanf("%c",&a);
+   if(a=='a'||a=='A'||a=='E'||a=='e'||a=='I'||a=='i'||a=='O'||a=='o'||a=='U'||a=='u')
+   printf("Vowel");
+   else
+   printf("Consonant");
+}

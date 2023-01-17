cc201836ffeb095e47d6beef62c0a138ca252b5b
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:09:00 +0530

    Create 19.C

---
+++ STRING PROGRAMS/19.C
@@ -0,0 +1,21 @@
+//Program to replace the first occurrence of a character in a string.
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    char j;
+    int i,p,k=0;
+    scanf("%[^\n]s",a);
+    scanf(" %c",&j);
+      for(i=0;a[i];i++)
+      k++;
+    for(i=0;a[i];i++)
+    {
+        if(a[i]==j)
+        {
+         a[i]='g';
+         break;
+        }
+    }
+    printf("%s",a);
+}

baae301a1421be26d3a18abc22b0aa08f4252251
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:13:55 +0530

    Create 1.C

---
+++ BASIC PROGRAMS/1.C
@@ -0,0 +1,11 @@
+//Program to perform input and output of all basic datatypes.
+#include<stdio.h>
+int main()
+{
+   int a;
+   float b;
+   char c;
+   double d;
+   scanf("%d %f %c %lf",&a,&b,&c,&d);
+   printf("a=%d b=%f c=%c d=%lf",a,b,c,d);
+}

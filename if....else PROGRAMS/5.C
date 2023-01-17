cf9e272bd977c6e130781ca598ac298d1dbcf9dc
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:37:18 +0530

    Create 5.C

---
+++ if....else PROGRAMS/5.C
@@ -0,0 +1,11 @@
+//Program to find whether a number is even or odd.
+#include<stdio.h>
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   if(a%2==0)
+   printf("Even");
+   else
+   printf("Odd");
+}

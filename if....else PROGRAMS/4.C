1756e585c6083a5c4fae731eb5ad74221288c678
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 19:36:52 +0530

    Create 4.C

---
+++ if....else PROGRAMS/4.C
@@ -0,0 +1,11 @@
+//Program to find whether a number is divisible by 5 and 11 or not.
+#include<stdio.h>
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   if(a%5==0&&a%11==0)
+   printf("Yes");
+   else
+   printf("No");
+}

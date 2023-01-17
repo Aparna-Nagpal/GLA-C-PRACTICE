1d89bf3eb27b6c98f3c44f1794ecdc1ddae4e549
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-17 18:49:57 +0530

    Create 18.C

---
+++ FUNCTION PROGRAMS/18.C
@@ -0,0 +1,20 @@
+//Program to find lcm of two numbers using functions.
+#include<stdio.h>
+int gcd(int m,int n)
+{
+    int i,k,l=0;
+    k=m>n?n:m;
+    for(i=k;i>0;i--)
+    {
+      l++;
+      if(m%i==0 && n%i==0)
+      break;
+    }
+    return(i);
+}
+void main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a*b/gcd(a,b));
+}

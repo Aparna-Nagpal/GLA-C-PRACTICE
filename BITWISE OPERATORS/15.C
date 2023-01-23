c767041528eca897dfb71c556330f857949feebd
Aparna-Nagpal <122882692+Aparna-Nagpal@users.noreply.github.com>
2023-01-23 19:00:17 +0530

    Create 15.C

---
+++ BITWISE OPERATORS/15.C
@@ -0,0 +1,9 @@
+//program to flip all the bits of a number using bitwise operator.
+#include <stdio.h>
+int main()
+{
+    int a,n,order=-1,i;
+    scanf("%d",&a);
+    printf("Flipped number=%d",~a);
+    return 0;
+}
